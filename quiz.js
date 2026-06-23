7336b92822b64c0c1c7353d8d855200f3a27889b
lark1410 <rapp.karl1410@gmail.com>
2026-06-23 09:58:27 +0200

    Implement score calculation for quiz
    This function calculates the score based on user input from a quiz form, displays the percentage score, and provides a custom message based on the score.

---
+++ quiz.js
@@ -0,0 +1,49 @@
+function calculerScore() {
+    let score = 0;
+    const totalQuestions = 5; 
+    const scoreMaxPossible = 15; 
+
+   
+    const form = document.getElementById('quizForm');
+    
+   
+    const q1 = form.querySelector('input[name="q1"]:checked');
+    const q2 = form.querySelector('input[name="q2"]:checked');
+    const q3 = form.querySelector('input[name="q3"]:checked');
+    const q4 = form.querySelector('input[name="q4"]:checked');
+    const q5 = form.querySelector('input[name="q5"]:checked');
+
+    
+    if (!q1 || !q2 || !q3 || !q4 ||!q5) {
+        alert("Fais pas le lâche, réponds à toutes les questions !");
+        return;
+    }
+
+ 
+    score += parseInt(q1.value);
+    score += parseInt(q2.value);
+    score += parseInt(q3.value);
+
+    
+    let pourcentage = Math.round((score / scoreMaxPossible) * 100);
+
+   
+    document.getElementById('pourcentage').innerText = pourcentage;
+    
+  
+    let message = "";
+    if (pourcentage === 100) {
+        message = "Tu es littéralement Playboi Carti. FÆHH ! 🧛💋";
+    } else if (pourcentage === 50) {
+        message = "Tu est déjà encré dans le style vamp mais continue encore sur ce chemin!!";
+    } else if (pourcentage <=70) {
+       message = "Tu as le style, tu as le flow. Un vrai soldat d'Opium! Il ne te manque plus grand chose pour atteindre ton paroxysme vamp"
+    } else {
+        message = "Tu es un random. Va écouter du Taylor Swift.";
+    }
+    
+    document.getElementById('phrase-custom').innerText = message;
+
+    
+    document.getElementById('resultat').classList.remove('hidden');
+}
