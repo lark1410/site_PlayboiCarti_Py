cceb2964eb7b74f4c1a915b06245d77e3a29bcf5
lark1410 <rapp.karl1410@gmail.com>
2026-06-25 11:48:35 +0200

    Update sound-shortcuts.js

--- sound-shortcuts.js
+++ sound-shortcuts.js
@@ -2,17 +2,17 @@ window.addEventListener('keydown', (event) => {
   const touche = event.key.toLowerCase();
 
   if (touche === 'c') {
-    const CARTII = document.getElementById('audio-album#1');
+    const CARTII = document.getElementById('audio-touche1');
     if (CARTII) { CARTII.currentTime = 0; CARTII.play().catch(e => {}); }
   } 
   
   else if (touche === 'f') {
-    const what = document.getElementById('audio-album#2');
+    const what = document.getElementById('audio-touche2');
     if (what) { what.currentTime = 0; what.play().catch(e => {}); }
   } 
   
   else if (touche === 's') {
-    const seyuh = document.getElementById('audio-album#3');
+    const seyuh = document.getElementById('audio-touche3');
     if (seyuh) { seyuh.currentTime = 0; seyuh.play().catch(e => {}); }
   }
 });
